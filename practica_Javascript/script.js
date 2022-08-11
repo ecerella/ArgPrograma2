2a784ba9acf10a99747e89fe36621c2881684500
e_man_chat <e_man_chat@hotmail.com>
2022-08-11 15:20:42 -0300

    actualizacion practica JavaScript

--- practica_Javascript/script.js
+++ practica_Javascript/script.js
@@ -290,3 +290,229 @@ function sumar (a, b) {
 sumar (5, 3); //no va a mostrar resultado, solo va a retornar un valor
 console.log(sumar(5, 3)); //valor 8 en consola
 
+//asignar un return
+function crearCadenaConMeta(lenguajeDeProgramacion){
+    return "mi meta es aprender " + lenguajeDeProgramacion;
+}
+var miMeta = crearCadenaConMeta("javascript");
+console.log(miMeta); //ahora si se muestra "mi meta es aprender javascript"
+
+//permanece en fila
+function proximoEnLaFila(arreglo, elemento) {
+    arreglo.push(elemento); //agregar al final del arreglo
+    return arreglo.shift(); //remover el primer elemento
+}
+var miArreglo = [1, 2, 3, 4, 5];
+console.log("antes: " + JSON.stringify(miArreglo)); //antes: [1, 2, 3, 4, 5]
+console.log(proximoEnLaFila(miArreglo, 6)); //muestra 1
+console.log("desp: " + JSON.stringify(miArreglo)); //desp: [2, 3, 4, 5, 6]
+
+//Boleanos true y false
+console.log(9 == 9); //true
+console.log(9 == "9"); //true representan el mismo numero
+// si queremos comparar los tipos de datos usamos igualdad estricta
+console.log(9 === 9); //true
+console.log(9 === "9"); //false no se convierten a dato comun
+
+//ejemplo
+var a;
+var b;
+
+a = 5;
+b = 5;
+
+console.log(a == b); //true
+console.log(a === b); //true
+
+b = 8;
+console.log (a == b); //false
+console.log (a === b); //false
+
+b = "5";
+console.log (a == b); //true
+console.log (a === b); //false
+
+//comparador d3esigualdad
+console.log(9 != 6); //true
+console.log(9 != 9); //false
+
+//comparador desigualdad estricta
+console.log(1 != "1"); //false
+console.log(1 !== "1"); //true esta comparando los tipos de dato
+
+//comparador mayor y menor
+console.log(6 > 5); //true
+console.log(3 > 10); //false
+
+console.log("b" > "a"); true//orden alfabetico
+console.log("acb"> "abc"); true//orden alfabetico 1ero y 2do caracter
+
+//comparador mayor o igual que
+console.log(5 > 5); //false
+console.log(5 >= 5); //true
+
+//opereadores logicos (condiciones elaboradas)
+/*
+tabla de verdad del operador AND
+para x && y (la operacion es true solo cuando los operandos son true)
+
+tabla de verdad del operador OR
+para x II y (si uno de los operandos es true el resultado es verdadero)
+
+tabla de verdad del operador NOT
+para x cambia la expresion
+var = 8;
+!(a > 5) --> !true --> false // la expresion a>5 es true pero NOT la cambia a false
+console.log(!(a > 5));
+*/
+
+//condicionales
+var x = 5;
+if (x > 2) {
+    console.log("la condicion es verdadera"); //se ejecuta solo si la condicion if es true
+}
+
+
+var x = 5;
+if (x < 2) {
+    console.log("la condicion es verdadera"); //no se ejecuta por la condicion falsa
+} else {
+    console.log("la condicion es falsa"); // se ejecuta
+}
+
+
+function clasificarValor(valor) {
+    if (valor % 2 == 0) { // si el valor dividido 2 tiene resto cero. se ejecuta
+        console.log("divisible entre 2");
+    } else if (valor % 3 == 0) {  //si el valor es divisible por 3 sin resto. se ejecuta
+        console.log("divisible entre 3");
+    } else {
+        console.log("no es divisible entre las opciones "); // si ninguna de las anteriores es true. se ejecuta
+    }
+}
+
+clasificarValor(2); //entrega divisible entre 2
+clasificarValor(15); // entrega divisible entre 3
+clasificarValor(7); // entrega no es divisible entre la opciones
+
+
+//interpretar indice de masa corporal
+function interpretarIMC(indiceDeMasaCorporal){
+    if (indiceDeMasaCorporal < 18.5){
+        console.log("bajo peso);")
+    } else if (indiceDeMasaCorporal <= 24.9) {
+        console.log("normal");
+    } else if (indiceDeMasaCorporal <= 29.9) {
+        console.log("sobrepeso");
+    } else {
+        console.log("obeso");
+    }
+}
+
+interpretarIMC(22.2); // entrega Normal
+
+//codigo de golf
+
+function codigoDeGolf(par, golpes) {
+    if (golpes == 1) {
+        return "hole-in-one!";
+    } else if (golpes <= par -2) {
+        return "eagle";
+    } else if (golpes == par -1) {
+        return "birdie";
+    } else if (golpes == par) {
+        return "par"; 
+    } else if (golpes == par + 1) {
+        return "bogey";
+    } else if (golpes == par + 2) {
+        return "double bogey";
+    } else if (golpes >= par + 3) {
+        return "go home!";
+    }
+}
+
+console.log(codigoDeGolf(4, 2)); //devuelve eagle
+console.log(codigoDeGolf(4, 4)); //devuelve par
+console.log(codigoDeGolf(4, 7)); //devuelve go home
+
+
+//sentencia switch a partir de un valor que va a pasar
+//es un condicional mas especifico, el valor de switch determina
+function clasificarValor(valor) {
+    var respuesta;
+    switch (valor) {
+        case 1: // si valor es igual a 1 pasara esto
+            respuesta = "alpha"; 
+            breack; //para la ejecucion de switch
+        case 2: // si valort es igual a 2 
+            respuesta = "beta";
+            breack;
+        case 3: // si valor es igual a 3
+            respuesta = "gamma";
+            breack;
+        case 4: // si valor es igual a 4
+            respuesta = "delta";
+            breack;
+    }
+    return respuesta;
+}
+
+console.log(clasificarValor (1)); //retorna alpha
+console.log(clasificarValor (4)); //retorna delta
+
+//suitch con cadenas de texto
+var producto = "hamburguesa";
+switch (producto){
+    case "pizza":
+        console.log("la pizza basica cuesta $10.55");
+        break;
+    case "hamburguesa":
+        console.log("las hamburguesas cuestan $6.78"); //se muestra en consola
+        breack;
+    case "helado":
+        console.log("el helado cuesta $2.8");
+        breack;
+}
+
+//sentencia switch, opcion predeterminada
+function seleccionarIdioma(valor){
+    var idioma;
+    switch (valor){
+        case 1:
+            idioma = "español";
+            breack;
+        case 2:
+            idioma = "frances";
+            breack;
+        case 3:
+            idioma = "italiano";
+            break;
+        default
+            idioma = "ingles";
+            breack;
+    }
+    return idioma;
+}
+
+console.log(seleccionarIdioma(1)); // español
+console.log(seleccionarIdioma(4)); // ingles ningun case tiene valor 4
+
+//sentencia switch, multiples casos
+function clasificarVolumen(valor) {
+    var volumen;
+    switch (valor){
+        case 1:
+            volumen = "bajo";
+            breack;
+        case 2:
+        case 3:
+            volumen = "intermedio";
+            break;
+        case 4:
+        case 5:
+        case 6:
+            volumen = "alto";
+            break;
+    }
+    return volumen;
+}
