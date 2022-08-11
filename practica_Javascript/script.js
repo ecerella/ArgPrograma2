623c023640f0ffbf89b62fe4ae6d610e86d0786c
e_man_chat <e_man_chat@hotmail.com>
2022-08-11 04:40:52 -0300

    agrego practica javascript

---
+++ practica_Javascript/script.js
@@ -0,0 +1,292 @@
+// comentario de una linea
+/* comentario de
+varias lineas
+*/
+// alert("hola mundo");
+
+ /*
+ tipos de datos:
+ undefined: sin valor asignado
+ null: nulo, objeto que no existe
+ boolean: true y false
+ string: cadenas de caracteres
+ symbol: 
+ number: numero
+ object: objetos (estructura que relaciona propiedades y funciones en un objeto)
+ */
+
+//variables
+ var miVariable="aprendo_java";
+ miVariable="Ezequiel";
+ console.log(miVariable)
+
+ var a;
+ var b=2;
+
+ console.log(a); //undefined, es una variable no inicializada
+ console.log(b); //2
+
+ var a=5;
+ var b=a; 
+ 
+ console.log(a)//5
+ console.log(b)//5
+
+/*suma, resta, multiplicacion y division de variables es positiva o negativa como cualquier operacion matematica tanto de enteros como decimales */
+
+//resto
+var resto = 5 % 3;
+console.log(resto); //2 seria lo que queda de la division 
+
+//incrementar una variable
+var librosLeidos = 10; 
+console.log(librosLeidos);
+
+//opcion 1 para incrementar variable
+librosLeidos = librosLeidos + 1;
+
+//opcion 2 para incrementar variable
+librosLeidos++; //se usa para incrementar de a 1 (para restar 1 uso --)
+
+var totalVentas = 13576.34;
+totalVentas = totalVentas + 345,67; //igual a decir (totalVentas += 345.676)
+console.log(totalVentas);
+//las 4 operaciones funcionan igual con asignacion compuesta
+
+//cadena de caracteres
+
+/*secuencias de escapes
+    codigo  resultado
+    \'      comilla simple. ej: var miVariable= 'estoy aprendiendo \'java\'';
+    \"      comilla doble. ej: var miVariable= "estoy aprendiendo \"java\"";
+    \\      barra invertida ej: var miVariable= "estoy aprendiendo \\java";
+    \n      linea nueva ej: console.log("estoy aprendiendo \njava");
+    \r      retorno de carro ej:
+    \t      tabulacion ej:
+    \b      retroceso ej:
+    \f      salto de pagina ej:
+*/
+
+//concatenar
+var mensajeCompleto = "estoy aprendiendo";
+var final = " a programar"
+console.log(mensajeCompleto); //entrega estoy aprendiendo
+
+mensajeCompleto += final;
+console.log(mensajeCompleto); //entrega estoy aprendiendo a programar
+
+//longitud de cadenas
+var cadena;
+cadena = "JavaScript";
+console.log(cadena.length); //entrega 10 (caraceres)
+
+//acceder a caracteres de cadena
+var lenguaje = "javaScript";
+/*cadena:  JavaScript
+  indices: 0123456789
+*/
+console.log(lenguaje[0]); //entrega la letra J
+
+//inmutabilidad (no se pueden cambiar)
+var miCadena = "Jola mundo";
+miCadena[0] = "H"; //type error, lo que si podemos es cambiar cadena completa
+
+var lenguaje = "javaScript";
+//cadena:  JavaScript
+//indices: 0123456789
+
+console.log(lenguaje[0]); //entrega la letra J
+console.log(lenguaje[3]); //entrega la letra a
+console.log(lenguaje[6]); //entrega la letra r
+console.log(lenguaje[9]); //entrega la letra t
+
+//ultimo caracter
+var miCadena;
+/*el ultimo indice siempre es longitud -1 (por que comenzamos en 0)
+miCadena.length para JavaScript es 10 (tiene 10 caracteres).
+el ultimo indice es 9.
+
+cadena:  JavaScript
+indices: 0123456789
+*/
+miCadena = "javaScript";
+console.log(miCadena[miCadena.length - 1]);
+
+//notacion de corchetes de derecha a izquierda
+var miCadena;
+/* el penultimo indice es longitud -2 (por que comenzamos en 0)
+miCadena = "JavaScript";
+n=2;
+console.log(miCadena[miCadena.length - n]); //entrega la letra p
+*/
+
+//juego historias locas
+/*
+var miSustantivo = "perro";
+var miAdjetivo = "negro";
+var miVerbo = "corrio";
+var miAdverbio = "rapidamente";
+
+/*concatena las cadenas para crear una que muestre un mensaje. puedes variar los valores de las variables.
+
+ej: el perro negro corrio rapidamente a la tienda.
+
+var palabrasEnBlanco = "el " + miSustantivo + " " + miAdjetivo + " " + miVerbo + " " + miAdverbio + " a la tienda."; //reemplazar comillas para armar concatenando
+console.log(palabrasEnBlanco);
+*/
+
+var miSustantivo = "bicicleta";
+var miAdjetivo = "pequeña";
+var miVerbo = "volo";
+var miAdverbio = "lentamente";
+
+//ej: la bicicleta pequeña volo a la tienda lentamente.
+
+var palabrasEnBlanco = "la " + miSustantivo + " " + miAdjetivo + " " + miVerbo + " a la tienda " + miAdverbio + "."; //reemplazar comillas para armar concatenando
+console.log(palabrasEnBlanco);
+
+//arreglos
+var miArreglo = ["jose", 24]
+console.log(miArreglo); //devuelve "jose", 24
+
+var estudiantes = ["pepe", "hector", "nora", "juan"];
+console.log(estudiantes); //devuelve "pepe", "hector", "nora", "juan"
+
+var notas = [95, 67, 89, 56];
+console.log(notas); //devuelve 95, 67, 89, 56
+
+//arreglos anidados
+var listaDeEstudiantes = [["nora", 97],["pepe", 78]];
+
+var listaDeProductos = [["camisa", 4.56, "cod134"], ["celular", 212, "cod32"]];
+
+var datos = [[3.5, 4.5, 2.1], [6.5, 2.3, 3.8], [6.2, 4.9, 6.0]];
+
+//acceder a elementos
+var miArreglo = [10, 20, 30];
+
+/*
+arreglo: [10, 20, 30]
+indices:   0   1   2
+*/
+
+console.log(miArreglo[0]); //10
+console.log(miArreglo[1]); //20
+console.log(miArreglo[2]); //30 
+
+//modificar un elemento en un arreglo
+var miArreglo = [10, 20, 30];
+
+miArreglo[0] = 40;
+console.log(miArreglo); //muestra 40, 20, 30
+
+//acceder a arreglo multidimensional
+var miArreglo = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+/*
+arreglo:        [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+indice:              0          1          2
+indice interno:   0  1  2    0  1  2    0  1  2
+*/
+console.log(miArreglo[0]); //muestra [1, 2, 3]
+console.log(miArreglo[1]); //muestra [4, 5, 6]
+
+//ahora supongamos que queremos acceder al arreglo 6
+console.log(miArreglo[1][2]); //muestra 6
+
+//.push (añade elemento al final de un elemento)
+var estaciones = ["invierno", "otoño", "primavera"];
+estaciones.push("verano");
+
+//.POP() remueve ultimo elemento
+var estaciones;
+estaciones = ["invierno", "otoño", "primavera", "verano"];
+estacion = estaciones.pop(); //elimina ultimo elemento y lo asigna a estaciones
+
+//.shift() remueve primer elemento
+estaciones = ["invierno", "otoño", "primavera", "verano"];
+estaciones.shift(); //elimina primer elemento
+
+//.unshift agrega al inicio
+estaciones = ["invierno", "otoño", "primavera"];
+estaciones.unshift("verano"); //agregar al principio
+
+var miListaDeCompras = [["cereal", 3], ["leche", 2], ["galletas", 4], ["panes", 5], ["gaseosas", 6], ["pollo", 7]];
+
+//cereal
+console.log("voy a comprar " + miListaDeCompras[0][1] + " unidades de " + miListaDeCompras[0][0] + ".");
+
+//leche
+console.log("voy a comprar " + miListaDeCompras[1][1] + " unidades de " + miListaDeCompras[1][0] + ".");
+
+//galletas
+console.log("voy a comprar " + miListaDeCompras[2][1] + " unidades de " + miListaDeCompras[2][0] + ".");
+
+//pollo
+console.log("voy a comprar " + miListaDeCompras[5][1] + " unidades de " + miListaDeCompras[5][0] + ".");
+
+//definir funciones
+function mostrarMensaje() {
+    console.log("hola mundo");
+}
+
+mostrarMensaje(); //llamamos a la funcion!
+
+function sumar(a,b) { //a y b son parametros
+    var suma = a + b;
+    console.log("el resultado de " + a + " + " + b + " es: " + suma);
+}
+
+sumar(5, 3); //entrega e l resultado de 5 + 3 es 8
+
+var x = 5;
+var y = 3;
+
+sumar(x, y); //paso las variables como argumento y el resultado es el mismo
+
+function concatenarTresCadenas(cadena1, cadena2, cadena3) {
+    console.log(cadena1 + " " + cadena2 + " " + cadena3);
+}
+concatenarTresCadenas("estoy", "aprendiendo", "javascript");
+
+//variables globales, se puede usar en cualquier parte del programa
+/*
+var miVariableGlobal = 5;
+console.log(miVariableGlobal); //devuelve 5
+
+function miFuncion() {
+    console.log(miVariableGlobal);
+}
+miFuncion(); //devuelve 5
+*/
+
+//variable local solo se usa dentro de la funcion donde se creo
+function miFuncion() {
+    var miVariableLocal = 4;
+    console.log(miVariableLocal);
+}
+
+miFuncion(); //devuelve 4
+
+console.log(miVariableLocal); //devuelve is not defined. declaro en la funcion
+
+//otro ejemplo
+var miNombre = "ezequiel";
+
+function mostrarMiNombre() {
+    var miNombre = "felipe";
+    console.log(miNombre);
+}
+
+mostrarMiNombre(); //devuelve felipe por que la variable local tiene prioridad
+
+console.log(miNombre); //devuelve ezequiel valor de la variable local
+
+//retornar funcion
+function sumar (a, b) {
+    return a + b;
+}
+
+sumar (5, 3); //no va a mostrar resultado, solo va a retornar un valor
+console.log(sumar(5, 3)); //valor 8 en consola
+
