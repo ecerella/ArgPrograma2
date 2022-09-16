d7eb532d2e82c2e656f53cf3985d9c974ed705e5
ecerella <ecerella@gmail.com>
2022-09-15 22:44:44 -0300

    sigo sumando codigo a carpeta dinamicoImagenes, todabia no funciona!

---
+++ dinamicoImagenes/main.js
@@ -0,0 +1,10 @@
+const grid = new Muuri('.grid', {
+    layout: {
+        rounding: false
+    }
+});
+
+window.addEventListener('load', () => {
+    grid.refreshItems().layout();
+    document.getElementById('grid').classList.add('imagenes-cargadas');
+});
