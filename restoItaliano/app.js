c6977e3c076a10498db1892bd52d092baf63de9f
ecerella <ecerella@gmail.com>
2022-09-27 14:38:59 -0300

    resto italiano, de la nonna, recien empiezo!

---
+++ restoItaliano/app.js
@@ -0,0 +1,43 @@
+
+
+const menu = document.querySelector('.hamburguesa');
+const navegacion = document.querySelector('.navegacion');
+
+document.addEventListener('DOMContentLoaded',()=>{
+    eventos();
+});
+
+const eventos = () =>{
+    menu.addEventListener('click',abrirMenu);
+}
+
+const abrirMenu = () =>{
+    navegacion.classList.remove('ocultar');
+    botonCerrar();
+}
+
+const botonCerrar = () =>{
+    const btnCerrar =document.createElement('p');
+    const overlay = document.createElement('div');
+    overlay.classList.add('pantalla-completa');
+    const body = document.querySelector('body');
+    if(document.querySelectorAll('.pantalla-completa').length > 0) return;
+    body.appendChild(overlay);
+    btnCerrar.textContent = 'x';
+    btnCerrar.classList.add('btn-cerrar');
+    navegacion.appendChild('btn-cerrar');
+    cerrarMenu(btnCerrar,overlay);
+}
+
+const cerrarMenu = (boton, overlay) =>{
+    boton.addEventListener('click',()=>{
+        navegacion.classList.add('ocultar');
+        overlay.remove();
+        boton.remove();
+    });
+    overlay.onclick = function(){
+        overlay.remove();
+        navegacion.classList.add('ocultar');
+        boton.remove();
+    }
+}
