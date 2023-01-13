b5a91b8708aa71213540229414eaca0a119e2111
ecerella <ecerella@gmail.com>
2023-01-13 04:51:21 -0300

    original springboot para desarrollo del frontend

---
+++ springbootBACK/src/main/java/com/portfolio/eze/EzeApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.eze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzeApplication.class, args);
+	}
+
+}
---
+++ springbootBACK/src/test/java/com/portfolio/eze/EzeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.eze;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
